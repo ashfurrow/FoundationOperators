a922b5e2f724778f24552c35f00cada116ff3f7c
Witek <jednymslowem@gmail.com>
2014-07-05 14:17:35 +0200

    pusty xcodeproj

---
+++ FoundationOperators/FoundationOperators/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  FoundationOperators
+//
+//  Created by Witek Przeciechowski on 05/07/2014.
+//  Copyright (c) 2014 Witek Przeciechowski. All rights reserved.
+//
+
+import Foundation
---
+++ FoundationOperators/FoundationOperators/FoundationOperators.h
@@ -0,0 +1,19 @@
+//
+//  FoundationOperators.h
+//  FoundationOperators
+//
+//  Created by Witek Przeciechowski on 05/07/2014.
+//  Copyright (c) 2014 Witek Przeciechowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoundationOperators.
+FOUNDATION_EXPORT double FoundationOperatorsVersionNumber;
+
+//! Project version string for FoundationOperators.
+FOUNDATION_EXPORT const unsigned char FoundationOperatorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoundationOperators/PublicHeader.h>
+
+
