fb449c26a6fb53daee86ff48dca6fcad52718b38
Witek <jednymslowem@gmail.com>
2014-07-05 17:52:24 +0200

    nsnumber

--- FoundationOperators/FoundationOperators/NSNumber.swift
+++ FoundationOperators/FoundationOperators/NSNumber.swift
@@ -23,3 +23,29 @@ import Foundation
 @infix func / (left: NSNumber, right: NSNumber) -> NSNumber {
     return left.doubleValue / right.doubleValue
 }
+
+@assignment func += (inout left: NSNumber, right: NSNumber) {
+    left = left.doubleValue + right.doubleValue
+}
+
+@assignment func -= (inout left: NSNumber, right: NSNumber) {
+    left = left.doubleValue - right.doubleValue
+}
+
+@assignment func *= (inout left: NSNumber, right: NSNumber) {
+    left = left.doubleValue * right.doubleValue
+}
+
+@assignment func /= (inout left: NSNumber, right: NSNumber) {
+    left = left.doubleValue / right.doubleValue
+}
+
+@prefix @assignment func ++ (inout operand: NSNumber) -> NSNumber {
+    operand = operand.doubleValue + 1.0
+    return operand
+}
+
+@prefix @assignment func -- (inout operand: NSNumber) -> NSNumber {
+    operand = operand.doubleValue - 1.0
+    return operand
+}
